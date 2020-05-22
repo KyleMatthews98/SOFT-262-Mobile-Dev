1c0669bf48acb7ac98ea8bdc38a0909ba3361b45
wjamie <jamie.walker@students.plymouth.ac.uk>
2020-05-22 16:27:15 +0100

    ListView
    1663 changed files...  what have I done

---
+++ SOFT262-FlashCards/SOFT262-FlashCards/QnA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOFT262_FlashCards
+{
+    public class QnA
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public QnA(string answer, string question) => (Question, Answer) = (question, answer);
+    }
+}
