5f1f5a1dd3f1a2ef59d63162b7f76739a1792346
wjamie <jamie.walker@students.plymouth.ac.uk>
2020-05-23 16:02:35 +0100

    AAAAAAHHHHHHH

--- SOFT262-FlashCards/SOFT262-FlashCards/MainPage.xaml.cs
+++ SOFT262-FlashCards/SOFT262-FlashCards/MainPage.xaml.cs
@@ -1,63 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-<<<<<<< Updated upstream
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-=======
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
->>>>>>> Stashed changes
 using Xamarin.Forms;
-using Microsoft.Azure.Cosmos;
 
 namespace SOFT262_FlashCards
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-<<<<<<< Updated upstream
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
-=======
         public static ObservableCollection<string> items { get; set; }
 
-
-        public static ObservableCollection<QnA> qnalist { get; set; }
-        public static ObservableCollection<string> qstrings { get; set; }
-        public static ObservableCollection<string> astrings { get; set; }
-
         public MainPage()
         {
-            QnAaccessors.getQnAlist();
-            QnAaccessors.getAs();
-            QnAaccessors.getQs();
-
-            qstrings = QnAaccessors.qlist;
-            astrings = QnAaccessors.qlist;
+            items = new ObservableCollection<string>() { "What's the capital of Germany?", "Who is the current UK Prime Minister?",
+                "Who won the World Cup in 1966", "What colour is an Orange?", "Who created the tech company'Apple'?", "What happens when you mix Coke and Mentos?",
+                "How many bits in a byte?", "What's the tallest mountain in the world named?", "How long should you brush your teeth for?", "What does MVVM stand for?" };
 
             ListView lstView = new ListView();
             lstView.IsPullToRefreshEnabled = true;
             lstView.Refreshing += OnRefresh;
             lstView.ItemSelected += OnSelection;
 
-            lstView.ItemsSource = qstrings;
+            lstView.ItemsSource = items;
             //lstView.SelectionMode = ListViewSelectionMode.None;
-          
+            var temp = new DataTemplate(typeof(textViewCell));
+            lstView.ItemTemplate = temp;
 
             Content = new StackLayout
             {
                 Margin = new Thickness(20),
                 Children =
                 {
-                    new Label { Text = "Flashcards", FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center },
+                    new Label { Text = "ListView Interactivity", FontAttributes = FontAttributes.Bold, HorizontalOptions = LayoutOptions.Center },
                     lstView
                 }
             };
@@ -71,18 +44,18 @@ namespace SOFT262_FlashCards
             {
                 return;
             }
-            DisplayAlert("Item Selected", e.SelectedItemIndex.ToString(), "Ok");
+            DisplayAlert("Item Selected", e.SelectedItem.ToString(), "Ok");
         }
 
         void OnRefresh(object sender, EventArgs e)
         {
             var list = (ListView)sender;
             //put your refreshing logic here
-            var itemList = qstrings.Reverse().ToList();
-            qstrings.Clear();
+            var itemList = items.Reverse().ToList();
+            items.Clear();
             foreach (var s in itemList)
             {
-                qstrings.Add(s);
+                items.Add(s);
             }
             //make sure to end the refresh state
             list.IsRefreshing = false;
@@ -100,13 +73,45 @@ namespace SOFT262_FlashCards
             var item = (MenuItem)sender;
             MainPage.items.Remove(item.CommandParameter.ToString());
         }
-
     }
 
 
 
-  
+    public class textViewCell : ViewCell
+    {
+        public textViewCell()
+        {
+            StackLayout layout = new StackLayout();
+            layout.Padding = new Thickness(15, 0);
+            Label label = new Label();
 
-    
->>>>>>> Stashed changes
+            label.SetBinding(Label.TextProperty, ".");
+            layout.Children.Add(label);
+
+            var moreAction = new MenuItem { Text = "More" };
+            moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+            moreAction.Clicked += OnMore;
+
+            var deleteAction = new MenuItem { Text = "Delete", IsDestructive = true }; // red background
+            deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+            deleteAction.Clicked += OnDelete;
+
+            this.ContextActions.Add(moreAction);
+            this.ContextActions.Add(deleteAction);
+            View = layout;
+        }
+
+        void OnMore(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            //Do something here... e.g. Navigation.pushAsync(new specialPage(item.commandParameter));
+            //page.DisplayAlert("More Context Action", item.CommandParameter + " more context action", 	"OK");
+        }
+
+        void OnDelete(object sender, EventArgs e)
+        {
+            var item = (MenuItem)sender;
+            MainPage.items.Remove(item.CommandParameter.ToString());
+        }
+    }
 }
---
+++ SOFT262-FlashCards/SOFT262-FlashCards/QnA.cs
@@ -0,0 +1,9 @@
+﻿namespace SOFT262_FlashCards
+{
+    public class QnA
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public QnA(string answer, string question) => (Question, Answer) = (question, answer);
+    }
+}
